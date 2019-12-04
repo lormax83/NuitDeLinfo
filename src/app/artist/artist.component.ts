624ecf524130732276e66aabd766e93b14eb89d6
Heron <M428943@airfrance.fr>
2019-12-04 17:22:02 +0100

    add album card

--- src/app/artist/artist.component.ts
+++ src/app/artist/artist.component.ts
@@ -9,24 +9,38 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ArtistComponent implements OnInit {
 
+  nbrAlbums:number = 0;
+  nbrSongs:number = 0;
+  listAlbums:any[] = [];
+  
   constructor(private http:HttpClient,private route: ActivatedRoute,) { }
   ngOnInit() {
     var id = this.route.snapshot.paramMap.get('id');
     var get = this.http.get('https://wasabi.i3s.unice.fr/api/v1/artist/name/'+id);
     get.subscribe((data: any[])=>{
-      console.log(data);
-      this.getAlbums(data);
+      this.getInfo(data);
     })
 
   }
 
-  getAlbums(id){
+  getInfo(id){
+    this.raz();
     var id = id._id;
     var get = this.http.get('https://wasabi.i3s.unice.fr/api/v1/artist_all/id/'+id);
-    get.subscribe((data: any[])=>{
-      console.log(data);
+    get.subscribe((data: any)=>{
+      this.nbrAlbums = data.albums.length;
+      for (let i = 0; i < data.albums.length; i++) {
+        this.nbrSongs = this.nbrSongs + data.albums[i].songs.length
+        if (data.albums[i].cover) {
+          this.listAlbums.push(data.albums[i]);
+        }
+      }
     })
     
+  }
+  raz(){
+    this.nbrAlbums = 0;
+    this.nbrSongs = 0;
   }
 
 }
